fe81c2765b9ffefcb3331d54ffeb976361128fb0
CommanderMark <markjarjour@gmail.com>
2019-05-31 18:01:09 -0400

    Moved formation of Mesh triangles to the Mesh constructor.

--- Src/Objects/Mesh.cpp
+++ Src/Objects/Mesh.cpp
@@ -6,7 +6,15 @@
 #include "../Math/Vector.h"
 #include "../Utils/OBJ_Loader.h"
 
-Mesh::Mesh(const Vector3f& diff, const Vector3f& spec, const Vector3f& amb, float shininessCoe) : Object(diff, spec, amb, shininessCoe) { }
+Mesh::Mesh(const std::vector<Vector3f> vertices, const std::vector<int> indices, const Vector3f& pos, const float scaleFactor, const Vector3f& diff, const Vector3f& spec, const Vector3f& amb, float shininessCoe) : Object(diff, spec, amb, shininessCoe), vertices(vertices) {
+    for (int i = 0; i < (int)indices.size(); i += 3) {
+        Vector3f p1 = vertices[indices[i]].multiply(scaleFactor).add(pos);
+        Vector3f p2 = vertices[indices[i + 1]].multiply(scaleFactor).add(pos);
+        Vector3f p3 = vertices[indices[i + 2]].multiply(scaleFactor).add(pos);
+
+        triangles.push_back(new Triangle(p1, p2, p3, diff, spec, amb, shininessCoe));
+    }
+}
 
 std::vector<Mesh*> Mesh::loadOBJ(const char* path, const Vector3f& pos, const float scaleFactor, const Vector3f& diff, const Vector3f& spec, const Vector3f& amb, float shininessCoe) {
     std::vector<Mesh*> retVal;
@@ -18,19 +26,17 @@ std::vector<Mesh*> Mesh::loadOBJ(const char* path, const Vector3f& pos, const fl
     if (loaded) {
         for (int i = 0; i < (int)Loader.LoadedMeshes.size(); i++) {
             objl::Mesh currMesh = Loader.LoadedMeshes[i];
-            retVal.push_back(new Mesh(spec, diff, amb, shininessCoe));
 
-            for (int j = 0; j < (int)currMesh.Vertices.size(); j++) {
-                retVal[i]->vertices.push_back(Vector3f(currMesh.Vertices[j].Position.X, currMesh.Vertices[j].Position.Y, currMesh.Vertices[j].Position.Z));
+            // Convert the array of indices from unsigned to signed integers.
+            std::vector<int> indices(currMesh.Indices.begin(), currMesh.Indices.end());
+
+            // Convert the array of vertices to our Vector type.
+            std::vector<Vector3f> verts;
+            for (int i = 0; i < (int)currMesh.Vertices.size(); i++) {
+                verts.push_back(Vector3f(currMesh.Vertices[i].Position.X, currMesh.Vertices[i].Position.Y, currMesh.Vertices[i].Position.Z));
             }
 
-            for (int j = 0; j < (int)currMesh.Indices.size(); j += 3) {
-                Vector3f p1 = retVal[i]->vertices[currMesh.Indices[j]].multiply(scaleFactor).add(pos);
-                Vector3f p2 = retVal[i]->vertices[currMesh.Indices[j + 1]].multiply(scaleFactor).add(pos);
-                Vector3f p3 = retVal[i]->vertices[currMesh.Indices[j + 2]].multiply(scaleFactor).add(pos);
-
-                retVal[i]->triangles.push_back(new Triangle(p1, p2, p3, diff, spec, amb, shininessCoe));
-            }
+            retVal.push_back(new Mesh(verts, indices, pos, scaleFactor, diff, spec, amb, shininessCoe));
         }
     } else {
         std::cerr << "Failed to Load File. May have failed to find it or it was not an .obj file.\n";
